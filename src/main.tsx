834b6fd0ae422167bfa15902892cf779257be832
adnansyed101 <sirgeant10@gmail.com>
2025-08-15 18:08:01 +0600

    Add react router and theme provider.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router";
+import Login from "./pages/auth/Login.tsx";
+import ThemeProvider from "./providers/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
