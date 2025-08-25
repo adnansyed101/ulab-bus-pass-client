f7b14c9ddc3c4edcea2ccf163103a27377d21817
adnansyed101 <sirgeant10@gmail.com>
2025-08-26 00:12:58 +0600

    Creat a type format.

---
+++ src/lib/types.tsx
@@ -0,0 +1,8 @@
+export type UserTripsCardProps = {
+  id: string;
+  destination: string;
+  remainingTrips: number;
+  departureTime: string;
+  pickupTime: string;
+  lastPurchaseDate: string;
+};
