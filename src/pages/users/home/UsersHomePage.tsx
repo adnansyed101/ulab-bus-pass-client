46c96956080727241308210ff0ce8f190a3e1cce
adnansyed101 <sirgeant10@gmail.com>
2025-09-08 20:34:12 +0600

    Covert into a page.
    Created a main layout for the website and changed this component into a page.

--- src/pages/users/home/UsersHomePage.tsx
+++ src/pages/users/home/UsersHomePage.tsx
@@ -1,11 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import ModeToggle from "@/components/shared/header/mode-toggle";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-dropdown-menu";
 import ShowCards from "./components/show-cards";
 import { BusSeatLayout } from "@/components/bus-seat-layout";
 
@@ -47,40 +40,25 @@ const UsersHomePage = () => {
 
   return (
     <>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
-              <ModeToggle />
-            </div>
-          </header>
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <h3 className="text-lg pl-4 font-medium tracking-wide">
-              Take a Trip.
-            </h3>
-            <div className="grid auto-rows-min gap-4 md:grid-cols-4">
-              <ShowCards cardData={remainingTrips} />
-            </div>
-            <div className="bg-muted/50 min-h-[100vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 flex-1 rounded-xl md:min-h-min">
-              {["Bus 1", "Bus 2", "Bus 3", "Bus 4"].map((bus) => (
-                <BusSeatLayout
-                  key={bus}
-                  occupiedSeats={[1, 3, 7, 12, 15, 18, 22, 28, 31, 35, 39]}
-                  onSeatSelect={(seatId) =>
-                    console.log(`Seat ${seatId} selected`)
-                  }
-                />
-              ))}
-            </div>
+      <h3 className="text-lg pl-4 font-medium tracking-wide">Take a Trip.</h3>
+      <div className="grid auto-rows-min gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-4">
+        <ShowCards cardData={remainingTrips} />
+      </div>
+
+      <Separator className="my-2" />
+
+      <div className="min-h-[100vh] grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4 flex-1 rounded-xl md:min-h-min">
+        {["Bus 1", "Bus 2", "Bus 3", "Bus 4"].map((bus) => (
+          <div key={bus}>
+            <h2 className="text-center text-2xl font-semibold mb-2">{bus}</h2>
+            <BusSeatLayout
+              key={bus}
+              occupiedSeats={[1, 3, 7, 12, 15, 18, 22, 28, 31, 35, 39]}
+              onSeatSelect={(seatId) => console.log(`Seat ${seatId} selected`)}
+            />
           </div>
-        </SidebarInset>
-      </SidebarProvider>
+        ))}
+      </div>
     </>
   );
 };
