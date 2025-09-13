a4da7945c0de56e7e23bbc79ac9dd9177d7c7e51
adnansyed101 <sirgeant10@gmail.com>
2025-09-13 16:11:18 +0600

    Add table for cart.

--- src/pages/users/buy-trips/copmponents/trips-cart.tsx
+++ src/pages/users/buy-trips/copmponents/trips-cart.tsx
@@ -1,4 +1,15 @@
-import { ShoppingBag } from "lucide-react";
+import { NumberInput } from "@/components/number-input";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoveRight, ShoppingBag, } from "lucide-react";
 
 const TripCart = () => {
   return (
@@ -7,6 +18,37 @@ const TripCart = () => {
         <ShoppingBag size={20} />{" "}
         <span className="text-xl font-medium tracking-wider">Cart</span>
       </h4>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Destination</TableHead>
+            <TableHead>Trip Amount</TableHead>
+            <TableHead>Total Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <span className="flex gap-2 items-center">
+                ULAB <MoveRight /> Dhanmondi 4/A
+              </span>
+            </TableCell>
+            <TableCell>
+              <NumberInput />
+            </TableCell>
+            <TableCell>TK 500</TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={2}>Total</TableCell>
+            <TableCell>TK 500</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+      <div className="flex justify-end mt-4">
+        <Button>Purchase</Button>
+      </div>
     </>
   );
 };
