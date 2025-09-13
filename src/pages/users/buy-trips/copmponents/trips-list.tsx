fa0a80eb9b83114e7b8e74b16510f06400a13d39
adnansyed101 <sirgeant10@gmail.com>
2025-09-13 16:12:09 +0600

    Add numberInput component, remove radio group and add table footer to show total cost.

--- src/pages/users/buy-trips/copmponents/trips-list.tsx
+++ src/pages/users/buy-trips/copmponents/trips-list.tsx
@@ -1,175 +1,58 @@
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { MoveRight, TicketPlus } from "lucide-react";
 
 const TripsList = () => {
   return (
-    <Table>
-      <TableCaption>List of destinations</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Id</TableHead>
-          <TableHead>Destination</TableHead>
-          <TableHead>Price / Trip</TableHead>
-          <TableHead>Single/Package</TableHead>
-          <TableHead>Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell>1</TableCell>
-          <TableCell>
-            <span className="flex gap-2 items-center">
-              ULAB <MoveRight /> Dhanmondi 4/A
-            </span>
-          </TableCell>
-          <TableCell>BDT 30</TableCell>
-          <TableCell>
-            <RadioGroup defaultValue="comfortable">
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="1"
-                  id="single"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="single">Single Trip</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="30"
-                  id="30"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="30">30 Trips</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="40"
-                  id="40"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="40">40 Trips</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="50"
-                  id="50"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="50">50 Trips</Label>
-              </div>
-            </RadioGroup>
-          </TableCell>
-          <TableCell>
-            <Button size="icon" className="size-8">
-              <TicketPlus />
-            </Button>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Id</TableCell>
-          <TableCell>Destination</TableCell>
-          <TableCell>BDT 30</TableCell>
-          <TableCell>
-            <RadioGroup defaultValue="comfortable">
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="1"
-                  id="single"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="single">Single Trip</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="30"
-                  id="30"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="30">30 Trips</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="40"
-                  id="40"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="40">40 Trips</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="50"
-                  id="50"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="50">50 Trips</Label>
-              </div>
-            </RadioGroup>
-          </TableCell>
-          <TableCell>
-            <Button size="icon" className="size-8">
-              <TicketPlus />
-            </Button>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Id</TableCell>
-          <TableCell>Destination</TableCell>
-          <TableCell>BDT 30</TableCell>
-          <TableCell>
-            <RadioGroup defaultValue="comfortable">
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="1"
-                  id="single"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="single">Single Trip</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="30"
-                  id="30"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="30">30 Trips</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="40"
-                  id="40"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="40">40 Trips</Label>
-              </div>
-              <div className="flex items-center gap-3">
-                <RadioGroupItem
-                  value="50"
-                  id="50"
-                  className="border-gray-400"
-                />
-                <Label htmlFor="50">50 Trips</Label>
-              </div>
-            </RadioGroup>
-          </TableCell>
-          <TableCell>
-            <Button size="icon" className="size-8">
-              <TicketPlus />
-            </Button>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <>
+      <h4 className="text-xl font-medium tracking-wider">Buy Trips</h4>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Destination</TableHead>
+            <TableHead>Price/Trip</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+            <TableCell>
+              <span className="flex gap-2 items-center">
+                ULAB <MoveRight /> Dhanmondi 4/A
+              </span>
+            </TableCell>
+            <TableCell>BDT 30</TableCell>
+            <TableCell className="flex items-center gap-2">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button size="icon" className="size-8">
+                    <TicketPlus />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Add to Cart</p>
+                </TooltipContent>
+              </Tooltip>
+              <Button variant="outline">Details</Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </>
   );
 };
 
