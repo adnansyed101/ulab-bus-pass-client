74c0f3ff5dc4d9011c599825fbf7499186a9256b
adnansyed101 <sirgeant10@gmail.com>
2025-09-13 16:10:14 +0600

    Remove title section.

--- src/pages/users/buy-trips/BuyTrips.tsx
+++ src/pages/users/buy-trips/BuyTrips.tsx
@@ -3,19 +3,14 @@ import TripCart from "./copmponents/trips-cart";
 
 const BuyTrips = () => {
   return (
-    <>
-      <h3 className="text-lg pl-4 font-medium tracking-wide">
-        Buy Trips, Have An Experience.
-      </h3>
-      <div className="min-h-[100vh] grid grid-cols-1 md:grid-cols-2 gap-2 flex-1 rounded-xl md:min-h-min ">
-        <div className="rounded-md px-4">
-          <TripsList />
-        </div>
-        <div className="rounded-md p-4">
-          <TripCart />
-        </div>
+    <div className="min-h-[100vh] grid grid-cols-1 md:grid-cols-2 gap-2 flex-1 rounded-xl md:min-h-min ">
+      <div className="rounded-md p-4">
+        <TripsList />
       </div>
-    </>
+      <div className="rounded-md p-4">
+        <TripCart />
+      </div>
+    </div>
   );
 };
 
