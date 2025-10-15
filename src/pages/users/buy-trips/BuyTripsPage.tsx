e8ec74b625fabb9bb581331803e3b7d1bbf9c5a8
adnansyed101 <sirgeant10@gmail.com>
2025-10-15 11:45:10 +0600

    Rename file.

---
+++ src/pages/users/buy-trips/BuyTripsPage.tsx
@@ -0,0 +1,11 @@
+import TripsList from "./copmponents/trips-list";
+
+const BuyTripsPage = () => {
+  return (
+    <div className="min-h-[100vh]">
+      <TripsList />
+    </div>
+  );
+};
+
+export default BuyTripsPage;
